1e50fc05d0acac2db715deb61403d5e27ab0cbef
Stefano Pigozzi <ste.pigozzi@gmail.com>
2018-12-01 18:46:41 +0100

    Add jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public float movementSpeed = 1f;
+    public float initialJumpForce = 10f;
+    public float pushDownOnReleaseForce = 5f;
+    public bool jumpAvailable = true;
+
+    private new Rigidbody2D rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate () {
+        float horizontalMovement = 0;
+        if(Input.GetKey(KeyCode.A)) {
+            horizontalMovement -= movementSpeed;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            horizontalMovement += movementSpeed;
+        }
+        if(jumpAvailable && Input.GetKeyDown(KeyCode.W))
+        {
+            jumpAvailable = false;
+            rigidbody.AddForce(Vector2.up * initialJumpForce);
+        }
+        if(Input.GetKeyUp(KeyCode.W))
+        {
+            rigidbody.AddForce(Vector2.down * pushDownOnReleaseForce);
+        }
+        rigidbody.velocity = new Vector3(horizontalMovement * movementSpeed, rigidbody.velocity.y);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Terrain"))
+        {
+            jumpAvailable = true;
+        }
+    }
 }
