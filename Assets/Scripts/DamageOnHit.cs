a831353f28545211066267047d5bab9cdbf6592c
Stefano Pigozzi <ste.pigozzi@gmail.com>
2018-12-01 17:52:57 +0100

    Add basic player

---
+++ Assets/Scripts/DamageOnHit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public int damage = 1;
+
+	// Quando il gameObject tocca qualcosa di solido...
+	void OnCollisionEnter2D (Collision2D collision) {
+		//Controlla se quello che ha colpito (collision.gameObject) è il giocatore (controlla tipo se il tag è "Player")
+        //Se sì, trova il suo componente healthController e chiamane il metodo .Damage(int danni)
+	}
+}
